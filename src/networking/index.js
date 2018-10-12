c1b11917ade75164d46fe4b9f09f67779fb2fbe0
Rodrigo Sosa <RodrigoSosa95@live.com.mx>
2018-10-11 21:36:24 -0500

    created file for each http request done through the app

---
+++ src/networking/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/todos/',
+});
+
+export const getTodos = () => instance.get();
+export const getTodo = id => instance.get(`${id}`);
+export const createTodo = payload => instance.post(payload);
+export const updateTodo = (id, payload) => instance.put(`${id}`, payload);
+export const partialUpdateTodo = (id, payload) => instance.patch(`${id}`, payload);
+export const deleteTodo = id => instance.delete(`${id}`);
